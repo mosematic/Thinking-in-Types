708b26b3ac6ae7dfc1939f9c90e71876ca7b9569
Mosematic <moselee10@gmail.com>
2022-10-07 23:00:38 +0300

    while loops

--- Flow_control/if_else.py
+++ Flow_control/if_else.py
@@ -5,14 +5,22 @@ zortan there is no age limit for getting a lecense.
 
 age: int = 13
 planet: str = "Earth"
+planet: str = "Zortan"
 
 # -------------------------
 #  And / Statement
 # --------------------------
 
 if age < 16 and planet == "Earth":
+    # Evaluation - True and True => True
     print("You are Not eligible for a license on earth")
+    # Evaluation stops here and we exit If/Else block
+    # ^---------------------------------------------^
 elif age > 16 and planet == "Earth":
+    # Execution does not reach here
+    # Evaluation - False and True => False
     print("You can apply for a license on Earth")
 elif age < 16 and planet == "Zortan":
+    # Execution does not reach here
+    # Evaluation - True or False => True
     print("You can apply for a Zortanian license!")
---
+++ Flow_control/print_name.py
@@ -0,0 +1,11 @@
+"""
+Moses that is in Zortan now, he has to do things differently.
+Zortan people spell their name with each character on different line.
+"""
+
+name: str = "Moses"
+
+# Create a 'For' loop to iterate over the characters in 'Moses'
+for character in name:
+    # Each iteration print a character
+    print(character)
